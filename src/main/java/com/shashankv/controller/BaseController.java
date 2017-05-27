b9e2add605deed4c2d5ad6834a9ac3b3e0623781
Shashank Vivek <shashank.093028@gmail.com>
2017-05-27 16:14:35 +0530

    adding more changes for bean injection.

--- src/main/java/com/shashankv/controller/BaseController.java
+++ src/main/java/com/shashankv/controller/BaseController.java
@@ -4,6 +4,7 @@ import com.shashankv.stock.dao.StockDao;
 import com.shashankv.stock.model.Stock;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,7 +22,9 @@ public class BaseController {
     private static int counter = 0;
 
     @Autowired
-    private StockDao stockDao;
+    @Qualifier("testAutowire")
+    StockDao stockDao;
+
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String welcome(ModelMap model) {
@@ -30,7 +33,7 @@ public class BaseController {
         model.addAttribute("counter", ++counter);
         logger.debug("[welcome] counter : {}", counter);
 
-        Stock stock = stockDao.findByStockCode("1");
+//        Stock stock = stockDao.findByStockCode("1");
         // Spring uses InternalResourceViewResolver and return back index.jsp
         return VIEW_INDEX;
 
--- src/main/java/com/shashankv/stock/dao/StockDao.java
+++ src/main/java/com/shashankv/stock/dao/StockDao.java
@@ -1,12 +1,14 @@
 package com.shashankv.stock.dao;
 
 import com.shashankv.stock.model.Stock;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by shashank on 14/5/17.
  */
-@Repository
+@Component("testAutowire")
 public interface StockDao {
     void save(Stock stock);
 
